00be4180846657f997211b0e90a5bb53c8ef1792
Naitian Zhou <nzhou2011@gmail.com>
2015-07-05 10:46:52 -0400

    fix: add votes column to ships table

--- database_setup.py
+++ database_setup.py
@@ -10,14 +10,12 @@ Base = declarative_base()
 database_path = 'sqlite:///shippah.db'
 
 
-class Ship(Base):
-    __tablename__ = 'ship'
+class Image(Base):
+    __tablename__ = 'image'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    user_id_1 = Column(Integer, ForeignKey(user.id), nullable=False)
-    user_1 = relationship(User)
-    user_id_2 = Column(Integer, ForeignKey(user.id), nullable=False)
-    user_2 = relationship(User)
+    x_coord = Column(Integer, nullable=False)
+    y_coord = Column(Integer, nullable=False)
+    path = Column(String(250), nullable=False)
 
 
 class User(Base):
@@ -26,6 +24,17 @@ class User(Base):
     name = Column(String(250), nullable=False)
     image_id = Column(Integer, ForeignKey(image.id))
     image = relationship(Image)
+
+
+class Ship(Base):
+    __tablename__ = 'ship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id_1 = Column(Integer, ForeignKey(user.id), nullable=False)
+    user_1 = relationship(User)
+    user_id_2 = Column(Integer, ForeignKey(user.id), nullable=False)
+    user_2 = relationship(User)
+    votes = Column(Integer)
 
 
 class Tag(Base):
@@ -42,13 +51,6 @@ class ShipTag(Base):
     tag_id = Column(String(250), ForeignKey(tag.id), nullable=False)
     tag = relationship(Tag)
 
-
-class Image(Base):
-    __tablename__ = 'image'
-    id = Column(Integer, primary_key=True)
-    x_coord = Column(Integer, nullable=False)
-    y_coord = Column(Integer, nullable=False)
-    path = Column(String(250), nullable=False)
 
 engine = create_engine(database_path)
 Base.metadata.create_all(engine)
