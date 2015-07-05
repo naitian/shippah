933e1c8281160d26b051c0c97397c74e03742544
Naitian Zhou <nzhou2011@gmail.com>
2015-07-05 09:41:23 -0400

    Add database to flask app.

---
+++ database_setup.py
@@ -0,0 +1,54 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+database_path = 'sqlite:///shippah.db'
+
+
+class Ship(Base):
+    __tablename__ = 'ship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id_1 = Column(Integer, ForeignKey(user.id), nullable=False)
+    user_1 = relationship(User)
+    user_id_2 = Column(Integer, ForeignKey(user.id), nullable=False)
+    user_2 = relationship(User)
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    image_id = Column(Integer, ForeignKey(image.id))
+    image = relationship(Image)
+
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class ShipTag(Base):
+    __tablename__ = 'ship_tag'
+    id = Column(Integer, primary_key=True)
+    ship_id = Column(String(250), ForeignKey(ship.id), nullable=False)
+    ship = relationship(Ship)
+    tag_id = Column(String(250), ForeignKey(tag.id), nullable=False)
+    tag = relationship(Tag)
+
+
+class Image(Base):
+    __tablename__ = 'image'
+    id = Column(Integer, primary_key=True)
+    x_coord = Column(Integer, nullable=False)
+    y_coord = Column(Integer, nullable=False)
+    path = Column(String(250), nullable=False)
+
+engine = create_engine(database_path)
+Base.metadata.create_all(engine)
