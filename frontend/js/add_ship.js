90e1ab8d4b009adbbc6474d5461b1c2838a9272f
unknown <TEC@TEC-W-IGMB-ST2.erdc.dren.mil>
2015-07-08 12:25:47 -0400

    Cleaned up the add ship dialog open animation

--- frontend/js/add_ship.js
+++ frontend/js/add_ship.js
@@ -1,20 +1,20 @@
 $('.add_ship_heart').click(function(){
-	$('.add_ship_dialog_path').css('top',($(this).position()['top'] + $(this).height() + 5))
+	if($('.add_ship_dialog').css('opacity') == 0){
+		$('.add_ship_dialog_path').css('top',($(this).position()['top'] + $(this).height() + 5))
+								 .css('left', ($(this).position()['left'] - $('.add_ship_dialog').width() + $(this).parent().width()) + 5)
+								 .animate({height: '285px'}, 250);
+		$('.add_ship_dialog').css('top',($(this).position()['top'] + $(this).height() + 20))
 							 .css('left', ($(this).position()['left'] - $('.add_ship_dialog').width() + $(this).parent().width()) + 5)
-							 .animate({height: '285px'}, 500);
-	$('.add_ship_dialog').css('top',($(this).position()['top'] + $(this).height() + 20))
-						 .css('left', ($(this).position()['left'] - $('.add_ship_dialog').width() + $(this).parent().width()) + 5)
-						 .animate({opacity: '1'}, 500);
-	/*if($('.add_ship_dialog').css('visibility') == 'hidden'){
-		console.log($(this))
-		$('.add_ship_dialog').css('visibility','visible')
-							 .css('top',($(this).position()['top'] + $(this).height() + 20))
-							 .css('left', ($(this).position()['left'] - $('.add_ship_dialog').width() + $(this).parent().width()) + 5);
-		$('.add_ship_triangle').css('visibility','visible')
-							   .css('left',$('.add_ship_dialog').width() - $('.add_ship_triangle').width() - 25)
-							   .css('bottom', $('.add_ship_dialog').height());
+							 .delay(125)
+							 .animate({opacity: '1'}, 250);
+		$('.add_ship_triangle').css('left',$('.add_ship_dialog').width() - $('.add_ship_triangle').width() - 25)
+							   .css('bottom', $('.add_ship_dialog').height())
+							   .delay(125)
+							   .animate({opacity: '1'}, 250);
 	} else {
-		$('.add_ship_dialog').css('visibility','hidden');
-		$('.add_ship_triangle').css('visibility','hidden');
-	}*/
+		$('.add_ship_dialog').animate({opacity: '0'}, 250);
+		$('.add_ship_triangle').animate({opacity: '0'}, 250);
+		$('.add_ship_dialog_path').delay(125)
+								  .animate({height: '0px'}, 250)
+	}
 });
