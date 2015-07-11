2fe82e7ee304de6f995bd1c75c057b0b7ff060b6
muakasan <muakasan@gmail.com>
2015-07-10 22:21:25 -0400

    added zoom

--- shippah_query.py
+++ shippah_query.py
@@ -40,3 +40,31 @@ def get_recent(num):
 				'tags': tags,
 			})
 	return serial_array
+
+def get_recent_tags(num, tagsList):
+	ships = session.query(Ship).order_by(desc(Ship.time)).limit(num).all()
+	serial_array =  []
+	for ship in ships:
+		tags = []
+		tags_query = session.query(ShipTag).filter_by(ship_id = ship.id).all()
+		for tag in tags_query:
+			tags.append(tag.tag.name)
+
+		serial_array.append({
+				'id': ship.id,
+				'name': ship.name.name,
+				'users': {
+					'user1': {
+						'name': ship.user_1.name.name,
+
+					},
+					'user2': {
+						'name': ship.user_2.name.name,
+
+					},
+				},
+				'time': ship.time,
+				'votes': ship.votes,
+				'tags': tags,
+			})
+	return serial_array
