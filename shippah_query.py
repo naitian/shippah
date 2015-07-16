bb2b68086a718a928f83fd2c14023306f01e9702
Naitian Zhou <nzhou2011@gmail.com>
2015-07-16 07:55:44 -0400

    Change shippah_query to not repeat code. Add ship-name gen.

--- shippah_query.py
+++ shippah_query.py
@@ -13,11 +13,11 @@ session = DBSession()
 
 
 def get_recent(num):
-	return session.query(Ship).order_by(desc(Ship.time)).limit(num).all()
+	return serialize(session.query(Ship).order_by(desc(Ship.time)).limit(num).all())
 	
 
 def get_popular(num):
-	return session.query(Ship).order_by(desc(Ship.votes)).limit(num).all()
+	return serialize(session.query(Ship).order_by(desc(Ship.votes)).limit(num).all())
 
 
 def get_tags(num, tagsList):
@@ -30,4 +30,31 @@ def get_tags(num, tagsList):
 		ships_temp_array += session.query(ShipTag).filter_by(tag_id=tag.id).all()
 	for ship in ships_temp_array:
 		ships.append(ship.ship)
-	return ships
+	return serialize(ships)
+
+def serialize(ships):
+	serial_array =  []
+	for ship in ships:
+		tags = []
+		tags_query = session.query(ShipTag).filter_by(ship_id = ship.id).all()
+		for tag in tags_query:
+			tags.append(tag.tag.name)
+
+		serial_array.append({
+				'id': ship.id,
+				'name': ship.name.name,
+				'users': {
+					'user1': {
+						'name': ship.user_1.name.name,
+
+					},
+					'user2': {
+						'name': ship.user_2.name.name,
+
+					},
+				},
+				'time': ship.time,
+				'votes': ship.votes,
+				'tags': tags,
+			})
+	return serial_array
