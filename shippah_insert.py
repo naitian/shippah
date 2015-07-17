64baaacc7147771787ec407dd686269dba5310b5
muakasan <muakasan@gmail.com>
2015-07-16 23:49:31 -0400

    edited database_setup insert to reflect not saving image coords. and zoom, removed classes and random files, query stilll not finished?

--- shippah_insert.py
+++ shippah_insert.py
@@ -16,34 +16,24 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def addShip(name, user_item_1, user_item_2, tags): #removed votes here
-	user_1, user_name_tag_1 = addUser(user_item_1)
-	user_2, user_name_tag_2 = addUser(user_item_2)
+def addShip(name, user_name_1, image_1, user_name_2, image_2, tags): 
 	ship_name_tag = addTag(name)
-	new_ship = Ship(name=ship_name_tag, user_1=user_1, user_2=user_2, time=datetime.now(), votes=0)
+	user_name_1 = addTag(user_name_1)
+	user_name_2 = addTag(user_name_2)
+
+	new_ship = Ship(name=ship_name_tag, user_1=User(user_name_1, image_1), user_2=User(user_name_2, image_2), time=datetime.now(), votes=0)
 	session.add(new_ship)
 
 	session.add(ShipTag(ship=new_ship, tag=ship_name_tag))
 	session.add(ShipTag(ship=new_ship, tag=user_name_tag_1))
 	session.add(ShipTag(ship=new_ship, tag=user_name_tag_2))
+	
 	for n in tags:
 		new_tag = addTag(n)
 		new_ship_tag = ShipTag(ship=new_ship, tag=new_tag)
 		session.add(new_ship_tag)
 	session.commit()
 
-
-def addUser(user):
-	if user.getImage():
-		image = user.getImage()
-		new_image = Image(x_coord=image.getX(), y_coord=image.getY(), zoom=image.getZoom(), path=image.getPath())
-		session.add(new_image)
-	else:
-		new_image=None
-	new_tag = addTag(user.getName())
-	new_user = User(name=new_tag, image=new_image)
-	session.add(new_user)
-	return new_user, new_tag
 
 def addTag(name):
 	try:
