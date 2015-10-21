edf904a8f0126e9ae960a4f5edc24adfb9794618
muakasan <muakasan@gmail.com>
2015-10-20 20:33:21 -0400

    deleted non front end files

--- database_test.py
+++
@@ -1,5 +0,0 @@
-from shippah_insert import *
-
-addShip('Naitina', User_Item('Naitian'), User_Item('Nina'), ['TJHSST', 'yolo', 'freshmen', 'adorbs'])
-addShip('Keatherine', User_Item('Catherine'), User_Item('Keaton'), ['TJHSST', 'junior', 'band', 'choircouple'])
-addShip('Heri', User_Item('Hayden'), User_Item('Meri'), ['TJHSST', 'yolo', 'junior', 'dota'])
--- shippah_crop.py
+++
@@ -1,7 +0,0 @@
-from os import remove
-from PIL import Image 
-def shippah_crop(originalFilePath, x1, y1, x2, y2, newFilePath):
-	original_img = Image.open(originalFilePath)
-	new_img = img.crop(x1, y2, x2, y2)
-	new_img.save(newFilePath)
-	os.remove(originalFilePath)
