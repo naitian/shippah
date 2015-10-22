293cd6f24957adacd1649347fdf7a350d8f7b2ca
muakasan <muakasan@gmail.com>
2015-10-21 20:33:01 -0400

    added image uploading capability

--- js/display_ships.js
+++ js/display_ships.js
@@ -1,30 +1,3 @@
-/*var cardTemplate = "" +
-"<div>" +
-	"<div class='card_header'>" +
-		"<div class='card_title'>" +
-			"<p>[SHIPNAME]</p>" +
-		"</div>" +
-	"</div>" +
-	"<div class='card_main'>" +
-		"<div class='card_persons'>" +
-			"<div class='card_person_1'>" +
-				"<div class='card_pic_1' style='background-url: url(\"[PIC_1]\"); background-position: [POS_X_1]% [POS_Y_1]%; background-size: [SCALE_1]% auto;'>" +
-				"</div>" +
-				"<div class='card_name_1'>" +
-					"<p>[NAME_1]</p>" +
-				"</div>" +
-			"</div>" +
-			"<div class='card_person_2' style='background-url: url(\"[PIC_1]\"); background-position: [POS_X_1]% [POS_Y_1]%; background-size: [SCALE_1]% auto;'>" +	
-				"<div class='card_pic_2'>" +
-				"</div>" +
-				"<div class='card_name_2'>" +
-					"<p>[NAME_2]</p>" +
-				"</div>" +
-			"</div>" +
-		"</div>" +
-	"</div>" +
-"</div>";*/
-
 var cardTemplate = "" +
 "<div id='[SHIP_ID]'>" +
 	"<div class='card_header'>" +
@@ -35,14 +8,14 @@ var cardTemplate = "" +
 	"<div class='card_main'>" +
 		"<div class='card_persons'>" +
 			"<div class='card_person_1'>" +
-				"<div class='card_pic_1' style='background-image: url(\"[PIC_1]\"); background-size: 200% auto;'>" +
+				"<div class='card_pic_1' style='[PIC_1]'>" +
 				"</div>" +
 				"<div class='card_name_1'>" +
 					"<p>[NAME_1]</p>" +
 				"</div>" +
 			"</div>" +
 			"<div class='card_person_2'>" +
-				"<div class='card_pic_2' style='background-image: url(\"[PIC_2]\"); background-size: 100% auto;'>" +
+				"<div class='card_pic_2' style='[PIC_2]'>" +
 				"</div>" +
 				"<div class='card_name_2'>" +
 					"<p>[NAME_2]</p>" +
@@ -54,10 +27,17 @@ var cardTemplate = "" +
 		"</div>" +
 	"</div>" +
 "</div>";
+var defaulImageStyle = 'background-image: url("http://www.aheritier.net/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png"); background-size: 100% auto;'
 
 function addShipToHTML(shipname, name_1, pic_1, name_2, pic_2, tags){
 	template = cardTemplate.slice(0);
-	console.log(shipname + ' | ' + name_1 + ' | ' + name_2);
+	if(pic_1===""){
+		pic_1 = defaulImageStyle;
+	}
+	if(pic_2===""){
+		pic_2 = defaulImageStyle;
+	}
+	//console.log(shipname + ' | ' + name_1 + ' | ' + name_2);
 	template = template.replace("[SHIPNAME]", shipname);
 	template = template.replace("[NAME_1]",name_1);
 	template = template.replace("[PIC_1]", pic_1);
--- js/firebase.js
+++ js/firebase.js
@@ -3,18 +3,33 @@ var myFirebaseRef = new Firebase("https://shippah.firebaseio.com/");
 
 myFirebaseRef.once("value", function(snapshot) {
   snapshot.forEach(function(ship) {
-    	addShipToHTML(ship.child("ship_name").val(), ship.child('user_name_1').val(), 'test1', ship.child('user_name_2').val(), 'test2', ship.child('tags').val());
+    	addShipToHTML(ship.child("ship_name").val(), ship.child('user_name_1').val(), ship.child('image_1').val(), ship.child('user_name_2').val(), ship.child('image_2').val(), ship.child('tags').val());
   });
 });
 
 $(document).on('click', '.add_ship_button input', function(){
 	rawTags = $('.add_ship_tags_input input').val();
 	tagsList = rawTags.replace('Tags:','').replace(/\s/g,'').split(',');
+	var image_1_style = $('.add_ship_pic_1').attr('style');
+	//default image style is in display_ships
+	if(image_1_style === defaulImageStyle){
+		image_1_style = "";
+	}
+
+	var image_2_style = $('.add_ship_pic_2').attr('style');
+	if(image_2_style === defaulImageStyle){
+		image_2_style = "";
+	}
+	console.log(image_1_style.indexOf('(')+1, image_1_style.lastIndexOf(')'));
 	myFirebaseRef.push({
 		ship_name: $('.add_ship_shipname input').val(),
 		user_name_1: $('.add_ship_name_1 input').val(),
 		user_name_2: $('.add_ship_name_2 input').val(),
+		image_1: image_1_style,
+		image_2: image_2_style,
 		tags: tagsList
+	}, function(){
+		location.reload();
+
 	});
-	location.reload();
 });
